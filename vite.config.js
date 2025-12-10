f3799af713db033abeeafd0a7e75b9ea051354e4
GothiccMiaurwick <sebastiansoler3@gmail.com>
2025-12-10 15:53:54 -0500

    change vite config?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  plugins: [react()],
-  base:'/Starbucks/',
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+
+  const repoName = process.env.VITE_REPO_NAME || 'Starbucks'
+  
+  return {
+    plugins: [react()],
+    // Usa el nombre del repositorio desde la variable de entorno
+    base: `/${repoName}/`,
+  }
 })
