ca0562bb950627707651223c3ba77ab1678a1893
GothiccMiaurwick <sebastiansoler3@gmail.com>
2025-12-10 19:52:38 -0500

    cambios en gitignore metiendo dist

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-
+export default defineConfig(({ command }) => {
   const repoName = process.env.VITE_REPO_NAME || 'Starbucks'
   
   return {
     plugins: [react()],
-    // Usa el nombre del repositorio desde la variable de entorno
-    base: `/${repoName}/`,
+    base: command === 'build' ? `/${repoName}/` : '/',
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      emptyOutDir: true
+    }
   }
 })
